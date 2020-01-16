53b6c55d4c91146e674a5107aa866d9c3c685a73
Sander De la Marche <sander@sumocoders.be>
2020-01-16 17:08:59 +0100

    Spaces

--- run.php
+++ run.php
@@ -12,15 +12,15 @@ function incrementCounter()
 
 function loop()
 {
-	$decode = "lame --decode src/input.mp3";
-	$encode = "lame --preset 128 --scale 1 src/input.wav src/input.mp3";
+    $decode = "lame --decode src/input.mp3";
+    $encode = "lame --preset 128 --scale 1 src/input.wav src/input.mp3";
 
-	exec($decode);
-	exec($encode);
+    exec($decode);
+    exec($encode);
 
-	/* We have to manually clean up the input.wav file after encoding it again. */
-	unlink('src/input.wav');
+    /* We have to manually clean up the input.wav file after encoding it again. */
+    unlink('src/input.wav');
 
-	/* Keep track of our iterations so we can display them on the page later */
+    /* Keep track of our iterations so we can display them on the page later */
     incrementCounter();
 }
